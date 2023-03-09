8928483aa68d1e579f1b390df80df4be35810b4c
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-09 09:17:16 +0200

    lisää etusivun pohjan

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Siru's Hairsaloon</title>
+    <link href="tyylit.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+     <h1>Siru's Hairsaloon</h1>
+     <nav>
+        <ul>
+          <li><a href="">Yhteystiedot</a></li>
+          <li><a href="">Pyydä tarjous</a></li>
+          <li><a href="">Palvelumme</a></li>
+          <li><a href="">Hinnasto</a></li>
+          <li><a href="">Meistä</a></li>
+        </ul>
+     </nav>
+    </header>
+     <main>
+       <h3>Tervetuloa kaunistautumaan!</h3>
+       <p>Tähän lyhyt kuvaus yrityksestä ja jotain kuvitusta?</p>
+    <div class="kuva kuva--oikea">       
+      <img src="https://via.placeholder.com/250x150" alt="">
+    </div>       
+    
+    <h2> TÄMÄN TILALLE TULEE LOMAKE KUN PHP TIEDOSTO ON OLEMASSA!</h2>
+      
+    </main>
+    <footer>
+      <p>Siru's Hairsaloon<br>
+      Kuviteltukatu 1<br>
+      00100 Helsinki<br>
+      +358501234567<br>
+      info@sirushairsaloon.fi<br><br><br>
+        &copy; Kurpitsa Solutions</p>
+    </footer>
+  </body>
+</html>
