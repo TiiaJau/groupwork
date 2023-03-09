46b34f59bd34f38500e10b4754b1acde31c25bfa
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-09 09:47:51 +0200

    lisää meista-sivun

---
+++ meista.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Meistä</title>
+    <link href="tyylit.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+     <h1>Siru's Hairsaloon</h1>
+     <nav>
+        <ul>
+          <li><a href="">Yhteystiedot</a></li>
+          <li><a href="">Pyydä tarjous</a></li>
+          <li><a href="">Palvelumme</a></li>
+          <li><a href="">Hinnasto</a></li>
+          <li><a href="">Meistä</a></li>
+        </ul>
+     </nav>
+    </header>
+     <main>
+       <h3>Siru' Hairsaloon, juuri sinun kauneuttasi varten!</h3>
+       <p>Tähän lyhyt kuvaus yrityksestä ja jotain kuvitusta?</p>
+    <div class="kuva kuva--oikea">       
+      <img src="https://via.placeholder.com/250x150" alt="">
+    </div>       
+    
+    <h3>Keitä olemme</h3>
+
+    <h4>Henri</h4>
+    <p>Tähän kuvaus ja alle joku kuva</p>
+    <div class="kuva kuva--oikea">       
+      <img src="https://via.placeholder.com/250x150" alt="">
+    </div> 
+
+    <h4>Julia</h4>
+    <p>Tähän kuvaus ja alle joku kuva</p>
+    <div class="kuva kuva--oikea">       
+      <img src="https://via.placeholder.com/250x150" alt="">
+    </div> 
+
+    <h4>Tiia</h4>
+    <p>Tähän kuvaus ja alle joku kuva</p>
+    <div class="kuva kuva--oikea">       
+      <img src="https://via.placeholder.com/250x150" alt="">
+    </div> 
+
+      
+    </main>
+    <footer>
+      <p>Siru's Hairsaloon<br>
+      Kuviteltukatu 1<br>
+      00100 Helsinki<br>
+      +358501234567<br>
+      info@sirushairsaloon.fi<br><br><br>
+        &copy; Kurpitsa Solutions</p>
+    </footer>
+  </body>
+</html>
