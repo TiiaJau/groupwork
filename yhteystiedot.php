8d8501ea0d122e45e58aa271248e3bb55cd912b9
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-09 10:27:49 +0200

    lisää yhteystiedot-sivun

---
+++ yhteystiedot.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Yhteystiedot</title>
+    <link href="tyylit.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+     <h1>Siru's Hairsaloon</h1>
+     <nav>
+        <ul>
+          <li><a href="index.php">Etusivu</a></li>
+          <li><a href="yhteystiedot.php">Yhteystiedot</a></li>
+          <li><a href="palvelumme.php">Palvelumme</a></li>
+          <li><a href="hinnasto.php">Hinnasto</a></li>
+          <li><a href="meista.php">Meistä</a></li>
+        </ul>
+     </nav>
+    </header>
+     <main>
+       <h2>Aukioloajat</h2>
+       <p><pre><br>
+       Maanantai - Perjantai            10-18<br>
+       Lauantai                         10-15<br>
+       Sunnuntai                        Suljettu<br></pre></p>
+       <h2>Yhteystiedot</h2>
+       <p>Siru's Hairsaloon<br>
+      Kuviteltukatu 1<br>
+      00100 Helsinki<br>
+      +358501234567<br>
+      info@sirushairsaloon.fi</p>
+
+      <p>Tähän ehkä jotain kuvaa?</p>
+    <div class="kuva kuva--oikea">       
+      <img src="https://via.placeholder.com/250x150" alt="">
+    </div>  
+
+    </main>
+    <footer>
+      <p>Siru's Hairsaloon<br>
+      Kuviteltukatu 1<br>
+      00100 Helsinki<br>
+      +358501234567<br>
+      info@sirushairsaloon.fi<br><br><br>
+        &copy; Kurpitsa Solutions</p>
+    </footer>
+  </body>
+</html>
