32ae9dcded5800f0cfc8ce189fb9b298628a2a87
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-09 10:42:14 +0200

    lisää palvelumme-sivun

---
+++ palvelumme.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Palvelumme</title>
+    <link href="tyylit.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+     <h1>Palvelumme</h1>
+     <nav>
+        <ul>
+          <li><a href="index.php">Etusivu</a></li>
+          <li><a href="yhteystiedot.php">Yhteystiedot</a></li>
+          <li><a href="palvelumme.php">Palvelumme</a></li>
+          <li><a href="hinnasto.php">Hinnasto</a></li>
+          <li><a href="meista.php">Meistä</a></li>
+        </ul>
+     </nav>
+    </header>
+     <main>
+     <h4>Hiusten leikkaukset ja värjäykset</h4>
+    <p>Meiltä saat kaikki perinteiset parturi- ja kampaamopalvelut.<br>
+    Toteutusten suhteen vain mielikuvituksesi on rajana ja teemmekin rohkeasti myös persoonallisia hiuksia.<br><br>
+
+    Saat meiltä myös hiuksillesi ja uudelle värillesi sopivat kotihoito-ohjeet ja tuotteet</p>      
+    
+    <h4>Kampaukset ja meikit</h4>
+    <p>TÄHÄN SISÄLTÖTEKSTIÄ!!!!</p>     
+    
+    <p>TÄHÄN MYÖS TARJOUSPYYNTÖ LOMAKE????<p>
+ 
+
+    </main>
+    <footer>
+      <p>Siru's Hairsaloon<br>
+      Kuviteltukatu 1<br>
+      00100 Helsinki<br>
+      +358501234567<br>
+      info@sirushairsaloon.fi<br><br><br>
+        &copy; Kurpitsa Solutions</p>
+    </footer>
+  </body>
+</html>
