702d418feb30c9902f8dd31b5613de37a3ceb967
Tiia Jauhiainen <tiia.jauhiainen@edu.sasky.fi>
2023-03-09 16:36:23 +0200

    lisää hinnasto-sivun

---
+++ hinnasto.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hinnasto</title>
+    <link href="tyylit.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+     <h1>Siru's Hairsaloon</h1>
+     <nav>
+        <ul>
+          <li><a href="index.php">Etusivu</a></li>
+          <li><a href="yhteystiedot.php">Yhteystiedot</a></li>
+          <li><a href="palvelumme.php">Palvelumme</a></li>
+          <li><a href="hinnasto.php">Hinnasto</a></li>
+          <li><a href="meista.php">Meistä</a></li>
+        </ul>
+     </nav>
+    </header>
+     <main>
+       <h2>HINNASTO</h2>
+       <p><pre><br>
+       <h4>Värjäykset</h4>
+       Tyviväri                         60 €<br>
+       Värjäys, lyhyet hiukset          70 €<br>
+       Värjäys, puolipitkät hiukset     85 €<br>
+       Värjäys, pitkät hiukset          100 €<br>
+       Raidat, lyhyet hiukset           70 €<br>
+       Raidat, puolipitkät hiukset      85 €<br>
+       Raidat, pitkät hiukset           100 €<br>
+       Extra pitkän/paksun hiuksen lisä +15 €<br><br>
+    
+       <h4>Kampaukset</h4>
+       Pikakampaus 30 min               30 €<br>
+       Kampaus, lyhyet hiukset          40 €<br>
+       Kampaus, puolipitkät hiukset     65 €<br>
+       Kampaus, pitkät hiukset          95 €<br>
+       Hääkampaus                       KYSY TARJOUS<br>
+       Extra pitkän/paksun hiuksen lisä +5 €<br><br>
+    
+       <h4>Meikkauspalvelut</h4>
+       Pikameikki                       35 €<br>
+       Päivämeikki                      45 €<br>
+       Juhlameikki                      68 €<br>
+       Häämeikki                        KYSY TARJOUS<br><br><br>
+    </pre></p>
+
+    </main>
+    <footer>
+      <p>Siru's Hairsaloon<br>
+      Kuviteltukatu 1<br>
+      00100 Helsinki<br>
+      +358501234567<br>
+      info@sirushairsaloon.fi<br><br><br>
+        &copy; Kurpitsa Solutions</p>
+    </footer>
+  </body>
+</html>
