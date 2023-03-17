d9a8c71193208c3e0971b5a4fa1dca969179085b
Henri I <128129550+henrigitgood@users.noreply.github.com>
2023-03-17 20:08:45 +0200

    Pääsivujen lisäys ja rakennemuutokset
    Muutoksina kansiorakenne ja sivujen sisältöjen muokkaus tyylimääritteisiin sopivaksi.

---
+++ src/view/yhteystiedot.php
@@ -0,0 +1,36 @@
+<main> 
+  <h2>Aukioloajat</h2>
+<div class='arrange-container'>
+  <p class='arrange-left-mp'> 
+    Arkipäivät
+    Lauantai                  
+    Sunnuntai       
+  </p>
+  <p class='arrange-right'>
+    10-18
+    10-15
+    Suljettu
+  </p>
+</div>
+  <div class='space-rm-div'></div>  
+  <hr>
+  <h2>Yhteystiedot</h2>
+    <p class='arrange-left'>
+      Siru's Hairsaloon
+      Kuviteltukatu 1
+      00100,Helsinki
+      +358501234567
+      info@sirushairsaloon.fi
+    </p>
+  <div class="yhteystiedot-kuva">       
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254497.1808326476!2d24.73782198905757!3d60.11020859364306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bc796210691%3A0xcd4ebd843be2f763!2sHelsinki!5e0!3m2!1sen!2sfi!4v1679015660254!5m2!1sen!2sfi" width="400" height="300" style="border: 2px solid #f8f8f89f; padding: 5px;background-color: #f5f5f5; border-radius: 1rem;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+  <div class='space-div'></div>
+  <hr>
+   <h2>Sivuston info</h2>
+    <p>
+     Sivut ovat luotu ja toteutettu vain koulutus tarkoituksessa.<br>
+     Kuvat ovat yhtä lukuunottamatta(lisenssin maininta kuvan yhteydessä) generoitu käyttäen ilmaista online-AI-kuvageneraattoria - Craiyon.<br>
+     Käytössä olevat kuvat ovat CC0-lisenssillä varustettuja.
+    </p> 
+</main>
